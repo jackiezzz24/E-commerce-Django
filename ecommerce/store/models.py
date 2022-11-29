fe13b2cd06c2bdfef755a8f2008000d0605c07c6
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2022-11-29 00:17:33 -0800

    build: base template, urls and views

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.urls import reverse
+
 # Create your models here.
 
 class Category(models.Model):
@@ -16,7 +18,16 @@ class Category(models.Model):
         
         return self.name
 
+
+    def get_absolute_url(self):
+
+        return reverse('list-category', args=[self.slug])
+        
+
 class Product(models.Model):
+
+    # FK
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
 
     title = models.CharField(max_length=250)
 
@@ -28,7 +39,7 @@ class Product(models.Model):
 
     price = models.DecimalField(max_digits=4, decimal_places=2)
 
-    #image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
 
     
     class Meta:
@@ -38,3 +49,7 @@ class Product(models.Model):
     def __str__(self):
         
         return self.title
+
+    def get_absolute_url(self):
+
+        return reverse('product-info', args=[self.slug])
