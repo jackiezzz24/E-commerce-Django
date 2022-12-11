95265c88e3610b8e5e823b536b737b140001fc61
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2022-12-11 00:33:33 -0800

    feat:implemented account feature

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     # Cart app
     path('cart/', include('cart.urls')),
     
+    # Account app
+    path('account/', include('account.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
